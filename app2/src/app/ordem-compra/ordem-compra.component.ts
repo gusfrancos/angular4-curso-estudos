edfc43519c5727a96b525afdcf778ba7a9e34300
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-07-26 19:35:45 -0300

    20210726

--- app2/src/app/ordem-compra/ordem-compra.component.ts
+++ app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -1,6 +1,6 @@
 import { CompileShallowModuleMetadata } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { OrdemCompraService } from '../ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
 
@@ -14,10 +14,10 @@ export class OrdemCompraComponent implements OnInit {
 
   
   public formulario: FormGroup = new FormGroup({
-    'endereco': new FormControl(null),
-    'numero': new FormControl(null),
-    'complemento': new FormControl(null),
-    'formaPagamento': new FormControl(null)
+    'endereco': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)] ),
+    'numero': new FormControl(null, [Validators.required, Validators.minLength(1), Validators.maxLength(20)]),
+    'complemento': new FormControl(null, [Validators.required, Validators.minLength(1), Validators.maxLength(20)]),
+    'formaPagamento': new FormControl(null, [Validators.required])
   })
 
   constructor(private ordemCompraService: OrdemCompraService) { }
