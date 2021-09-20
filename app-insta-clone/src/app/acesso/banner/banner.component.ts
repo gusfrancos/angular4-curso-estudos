a8e2fa2c5acb6a8fb19a7467e3b154df0afcd98c
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2021-09-20 20:33:11 -0300

    Animations, trigger, states e styles

--- app-insta-clone/src/app/acesso/banner/banner.component.ts
+++ app-insta-clone/src/app/acesso/banner/banner.component.ts
@@ -1,15 +1,34 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity: 1
+      })),
+      transition('escondido <=> visivel', animate('3s ease-in'))
+      
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
+
+  public estado: string = 'visivel'
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public toggleEstado(): void {
+    this.estado = this.estado === 'visible' ? 'escondido' : 'visible';
   }
 
 }
